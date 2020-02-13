54a7d8b8d41bbaf28e7dba9438577c4cb0ca0dcb
Steven Doran <steven.doran@flatironschool.com>
2020-02-13 13:08:20 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react'
+
+export default class EyesOnMe extends Component {
+  render(){
+    return (
+      <button onFocus={this.printGood} onBlur={this.printEyes}></button>
+    )
+  }
+
+  printGood(){
+    console.log('Good!')
+  }
+
+  printEyes(){
+    console.log('Hey! Eyes on me!')
+  }
+}
